eeeb5963afd9eedd2f374cf2b40227ae4fdec71e
Alex Balint <Alex.Balint@qubiz.local>
2023-05-16 11:42:53 +0300

    asdasd

--- cypress/e2e/3-custom/spec1.cy.js
+++ cypress/e2e/3-custom/spec1.cy.js
@@ -1,28 +1,28 @@
-describe("Simple Form Demo", () => {
+describe('Simple Form Demo', () => {
   beforeEach(() => {
-    cy.setCookie("exit_popup_dismissed", "closed");
-    cy.visit("https://www.lambdatest.com/selenium-playground/simple-form-demo");
-  });
+    cy.setCookie('exit_popup_dismissed', 'closed')
+    cy.visit('https://www.lambdatest.com/selenium-playground/simple-form-demo')
+  })
 
-  it("Types into single input field", () => {
-    cy.get("#user-message").type("Some kind of text");
+  it('Types into single input field', () => {
+    cy.get('#user-message').type('Some kind of text')
 
-    cy.get("#showInput").click();
+    cy.get('#showInput').click()
 
-    cy.get("#message").should("have.text", "Some kind of text");
-  });
+    cy.get('#message').should('have.text', 'Some kind of text')
+  })
 
-  it("Adds two numbers", () => {
-    cy.get("#sum1").type("100");
-    cy.get("#sum2").type("899");
-    cy.contains("Get values").click();
+  it('Adds two numbers', () => {
+    cy.get('#sum1').type('100')
+    cy.get('#sum2').type('899')
+    cy.contains('Get values').click()
 
-    cy.get("#addmessage").should("have.text", "999");
+    cy.get('#addmessage').should('have.text', '999')
 
-    cy.get("#sum1").clear();
-    cy.get("#sum1").type("Text");
-    cy.contains("Get values").click();
+    cy.get('#sum1').clear()
+    cy.get('#sum1').type('Text')
+    cy.contains('Get values').click()
 
-    cy.get("#addmessage").should("have.text", "NaN");
-  });
-});
+    cy.get('#addmessage').should('have.text', 'NaN')
+  })
+})
